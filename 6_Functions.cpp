c2fe3647f135e1bd4da5173615b6989812f430b6
Krishna <kv.vermakrishna@gmail.com>
2024-09-14 01:19:59 +0530

    Basic Language

--- 6_Functions.cpp
+++ 6_Functions.cpp
@@ -8,22 +8,20 @@ void func(string &str1) { // Same can be done with integer and other data types
     cout << str1 << '\n';
 }
 // The '&' sign tells to take the address of the variable passed
+// To pass a value by reference, there are other methods as well
 
 
 int main() {
-    // Even the string data type is passed by value inside a function
+    // Define function as
+    // void func(int arr[]) { ... }
 
-
-
-
+    
+    // Even the string data type is passed by value inside a function, ie, a copy of that string is passed to the function
     string str = "raj";
     func(str);  // Simply pass the variable here
     cout << str << '\n';
 
-    // Array is generally passed by reference
-    // Define function as
-    // void func(int arr[]) { ... }
-    
+    // Array is generally passed by reference    
 }
 
 
